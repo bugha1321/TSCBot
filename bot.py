e571ffe0f1f8276931cdbb47353a15018b22b1d9
bugha1321 <75084149+bugha1321@users.noreply.github.com>
2022-08-05 10:25:29 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,4 +12,4 @@ async def on_ready():
 async def Info(ctx):
     await ctx.channel.send("Test")
 
-client.run("MTAwNDczODk0MDcwOTkwODQ5MA.G-DCy7.54wiLgXO-7JJHEgYpjimASB1eh2279vRnNvEKA")
+client.run("MTAwNTA1ODcwMzI2NDg2MjIzOQ.G9l8r2.IPMI6uIdmrnxzAGkzGwgYEJnqz0JAqpK8mBKx0")
